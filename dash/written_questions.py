4aa9ef78949ebf722516ed983a6c63870d32e209
NielsTack_MXLinux <niels_tack@hotmail.com>
2024-01-30 13:35:14 +0100

    implement styling + DatePickerRange()

--- dash/written_questions.py
+++ dash/written_questions.py
@@ -18,6 +18,8 @@ from attendance_statistics import get_party
 # =============================================================================
 # Load df with written questions and information about parties
 written_questions_df = pd.read_pickle('../data/details_questions_term_df.pkl')
+# Create a default value for amount_meetings, i.e. relevant meetings
+amount_questions = len(written_questions_df)  # Set amount of all meetings as default, it will be updated in the callback
 
 # Load information about parties
 with open('../data/fracties.pkl', 'rb') as file:
@@ -59,39 +61,113 @@ minister_colors = {
 # =============================================================================
 
 # Dash app
-app = dash.Dash(__name__)
+app = dash.Dash(__name__,
+                assets_folder='assets') # Relative path to the folder of css file))
 
 # =============================================================================
 # Dash layout
 # =============================================================================
 
-app.layout = html.Div([
-    html.H1("Schriftelijke vragen"),
-    dcc.Dropdown(
-        id='x-axis-dropdown',
-        options=[
-            {'label': 'Vraagsteller', 'value': 'vraagsteller'},
-            {'label': 'Partij', 'value': 'partij'},
-            {'label': 'Minister', 'value': 'minister'},
-        ],
-        value='vraagsteller', # Default value
-        style={'width': '50%'}
-    ),
-    dcc.Graph(id='bar-chart')
-])
+## Comment in integrated approach
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.H2("Schriftelijke vragen",
+                        className="header-subsubtitle"),
+                html.P("Welke parlementsleden stelden het meeste parlementaire vragen? En welke ministers kregen de meeste vragen te verwerken?",
+                       className="header-description"),
+            ],
+            className="section-header",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.H3(
+                            "Selecteer de relevante periode.",
+                            className="header-subsubtitle",
+                        ),
+                        html.Div(
+                            children=[
+                                html.Div(
+                                    children="Relevante periode", 
+                                    className="menu-title"
+                                ),
+                    				# html.Div(
+                    				# 	# Display the most recent meeting date
+                    				# 	id='most-recent-date-per-party',
+                    				# 	children=f"Laatste update: {date_most_recent_meeting_per_party}",
+                    				# 	style={'font-style': 'italic'}
+                    				# ),
+                                dcc.DatePickerRange(
+                                    id="date-range-written-questions",
+                                    min_date_allowed=written_questions_df["datum"].min(),
+                                    max_date_allowed=written_questions_df["datum"].max(),
+                                    start_date=written_questions_df["datum"].min(),
+                                    end_date=written_questions_df["datum"].max(),
+                                    display_format='DD/MM/YYYY',  # Set the display format to 'dd/mm/yyyy' instead of default 'mm/dd/yyyy'
+                                ),
+                            ],
+                            className="menu-element"
+                        ),
+                    		# Display impact of data selection (i.e. how many written questions are taken into account)	
+                    		html.Div(
+                    			children=[
+                    				html.Div(id='amount_questions',
+                    						 children=f"Deze selectie resulteert in {amount_questions} schriftelijke vragen."),
+                    			],
+                    			className="menu-element"
+                    		),
+                    ], 
+                    className="section-chart",
+                ),
+            ],
+            className="wrapper",
+        ),
+    
+        dcc.Dropdown(
+            id='x-axis-dropdown',
+            options=[
+                {'label': 'Vraagsteller', 'value': 'vraagsteller'},
+                {'label': 'Partij', 'value': 'partij'},
+                {'label': 'Minister', 'value': 'minister'},
+            ],
+            value='vraagsteller', # Default value
+            style={'width': '50%'}
+        ),
+        dcc.Graph(id='written_questions_graph'),
+    ],
+    # use CSS flexbox approach to easily structure graphs and titles
+    style={"display": "flex", "flex-direction": "column"} 
+)
+
+
 
 # =============================================================================
 # Callback
 # =============================================================================
+#Define function to filter data based on user selection
+def filter_data(start_date, end_date, written_questions_df):   
+    # Ensure correct date format
+    start_date = pd.to_datetime(start_date).date()
+    end_date = pd.to_datetime(end_date).date()
+    
+    
+    # Filter DataFrame with all commission meetings further based on the date range
+    written_questions_filtered_df = written_questions_df[
+         (written_questions_df['datum'] >= start_date) &
+         (written_questions_df['datum'] <= end_date)
+     ]
+    
+    return written_questions_filtered_df
 
-@app.callback(
-    Output('bar-chart', 'figure'),
-    [Input('x-axis-dropdown', 'value')]
-)
-def update_chart(selected_axis):
+
+def update_chart(selected_axis, written_questions_df_input):
+  
     if selected_axis == 'vraagsteller':      
         # Create a DataFrame with member, count, and party columns
-        grouped_data = written_questions_df['vraagsteller'].value_counts().reset_index()
+        grouped_data = written_questions_df_input['vraagsteller'].value_counts().reset_index()
         grouped_data.columns = ['Parlementslid', 'Aantal vragen']
 
         # Apply function to create a new column 'Partij' based on fracties_dict
@@ -110,7 +186,7 @@ def update_chart(selected_axis):
         fig.update_xaxes(categoryorder='total descending')
 
     elif selected_axis == 'minister':
-        grouped_data = written_questions_df['minister'].value_counts().reset_index()
+        grouped_data = written_questions_df_input['minister'].value_counts().reset_index()
         grouped_data.columns = ['Minister', 'Aantal vragen']
         fig = px.bar(grouped_data,
                      x='Minister',
@@ -122,7 +198,7 @@ def update_chart(selected_axis):
 
     elif selected_axis == 'partij':
 
-        grouped_data = written_questions_df['vraagsteller_partij'].value_counts().reset_index()
+        grouped_data = written_questions_df_input['vraagsteller_partij'].value_counts().reset_index()
         grouped_data.columns = ['Partij', 'Aantal vragen']
         fig = px.bar(grouped_data,
                      x='Partij',
@@ -136,6 +212,30 @@ def update_chart(selected_axis):
         fig = px.bar()
 
     return fig
+
+
+@app.callback(
+    [Output('amount_questions', 'children'),
+     Output('written_questions_graph', 'figure')],
+    [Input('date-range-written-questions', 'start_date'),
+     Input('date-range-written-questions', 'end_date'),
+     Input('x-axis-dropdown', 'value')]
+    )
+def update_display(start_date, end_date, selected_axis):
+    # Filter data based on user input
+    written_questions_filtered_df = filter_data(start_date, end_date, 
+                                                written_questions_df)
+	# Obtain count of relevant data, after filtering
+    amount_questions = len(written_questions_filtered_df)
+    
+    # Create graph using user selected axis and filtered df
+    written_questions_graph = update_chart(selected_axis, 
+                                           written_questions_filtered_df)
+
+    return [f"Deze selectie resulteert in {amount_questions} relevante vergaderingen.", # Use text formatting to allow easier build of layout
+				written_questions_graph]
+
+
 
 # =============================================================================
 # Run dash app
